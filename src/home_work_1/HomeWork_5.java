03093fddfd3814c4478883fa6b71adb7023ae95d
Deam_on <Teamoha@yandex.ru>
2024-02-22 15:24:46 +0300

    Добавил 5 задание

---
+++ src/home_work_1/HomeWork_5.java
@@ -0,0 +1,55 @@
+package home_work_1;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+//Создать СТАТИЧЕСКИЙ метод sleepIn рядом с методом main. (Взято с https://codingbat.com/prob/p187868).
+//	5.1 Данный метод будет принимать два параметра
+//	5.2 Будет отвечать на вопрос спать ли дальше (да\нет) (возвращать true либо false).
+//	5.3 Первый параметр boolean weekday обозначает рабочий день
+//	5.4 Второй параметр boolean vacation обозначает отпуск.
+//	5.5 Если у нас отпуск или не рабочий день то мы можем спать дальше
+//	5.6 На основании ответа от метода sleepIn вывести сообщение можем спать дальше или пора идти на работу
+public class HomeWork_5 {
+    static boolean a;
+    static boolean b;
+    public static void main(String[] arg) {
+        System.out.println("Сегодня рабочий день? Да/Нет?");
+        Scanner ans = new Scanner(System.in);
+        String yesNo1 = ans.nextLine();
+        if (Objects.equals(yesNo1, "Да")) {
+            a = true;
+        }
+        if (Objects.equals(yesNo1, "Нет")) {
+            a = false;
+            System.out.println("Можешь спать дальше");
+            return;
+        }
+        System.out.println("Ты в отпуске? Да/Нет?");
+        Scanner ans1 = new Scanner(System.in);
+        String yesNo2 = ans1.nextLine();
+        if (Objects.equals(yesNo2, "Да")) {
+            b = true;
+        }
+        if (Objects.equals(yesNo2, "Нет")) {
+            b = false;
+        }
+        if (sleepIn(a,b)) {
+            System.out.println("Можешь спать дальше");
+        } else {
+            System.out.println("Пора идти на работу");
+        }
+        if (Objects.equals(yesNo2, "Да")) {
+            b = true;
+        }
+        if (Objects.equals(yesNo2, "Нет")) {
+            b = false;
+        }
+    }
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        }
+        return false;
+    }
+}
