746eb62c2740a191bad8060910ed14b428b624fa
Deam_on <Teamoha@yandex.ru>
2024-04-16 23:38:19 +0300

    убрал сканнеры из ДЗ2

--- src/home_work_2/loops/Main.java
+++
@@ -1,7 +0,0 @@
-package home_work_2.loops;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(OperationOnNumbers.countEvenOdd(25468294));
-    }
-}
--- src/home_work_2/loops/OperationOnNumbers.java
+++ src/home_work_2/loops/OperationOnNumbers.java
@@ -52,115 +52,40 @@ public class OperationOnNumbers {
         return "Чётных цифр: " + even + "; " + "Нечётных цифр: " + odd;
     }
 
-    public static void countRowFibonacci() {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Введите количество элементов ряда Фибоначчи для вывода на экран:");
-        int n = scn.nextInt();
-        int a, b, c;
-        if (n == 1) {
-            System.out.println("Первый элемент ряда Фибоначчи это: 0");
-        } else {
-            a = 0;
-            b = 1;
-            System.out.print("Первые " + n + " элемента(ов) ряда Фибоначчи это: " + a + " " + b + " ");
-            for (int i = 1; i < n - 1; i++) {
+    public static String countRowFibonacci(int countElementsOfRow) {
+        int a = 0, b = 1, c;
+        StringBuilder row = new StringBuilder();
+        for (int i = 0; i < countElementsOfRow; i++) {
+            if (i == 0) {
+                row.append(a).append(" ");
+            } else if (i == 1) {
+                row.append(b).append(" ");
+            }else {
                 c = a + b;
                 a = b;
                 b = c;
-                System.out.print(c + " ");
+                row.append(c).append(" ");
             }
         }
-        System.out.println(" ");
+        return row.toString();
     }
 
-    public static void showRowWithStep() {
-        String naturalNum1;
-        int length;
-        char k = 0;
-        do {
-            Scanner scn = new Scanner(System.in);
-            System.out.println("Введите целое положительное число - начало диапазона (минимум):");
-            naturalNum1 = scn.nextLine();
-            length = naturalNum1.length();
-            for (int i = 0; i < length; i++) {
-                k = naturalNum1.charAt(i);
-                if (!Character.isDigit(k)) {
-                    System.out.println("Введены некорректные данные");
-                    break;
-                }
-            }
-        } while (!Character.isDigit(k));
-
-        String naturalNum2;
-        int length2;
-        char k2 = 0;
-        do {
-            Scanner scn = new Scanner(System.in);
-            System.out.println("Введите целое положительное число - конец диапазона (максимум):");
-            naturalNum2 = scn.nextLine();
-            length2 = naturalNum2.length();
-            for (int i = 0; i < length2; i++) {
-                k2 = naturalNum2.charAt(i);
-                if (!Character.isDigit(k2)) {
-                    System.out.println("Введены некорректные данные");
-                    break;
-                }
-            }
-        } while (!Character.isDigit(k2));
-
-        String naturalStep;
-        int lengthStep;
-        char k3 = 0;
-        do {
-            Scanner scn = new Scanner(System.in);
-            System.out.println("Введите целое положительное число - шаг:");
-            naturalStep = scn.nextLine();
-            lengthStep = naturalStep.length();
-            for (int i = 0; i < lengthStep; i++) {
-                k3 = naturalStep.charAt(i);
-                if (!Character.isDigit(k3)) {
-                    System.out.println("Введены некорректные данные");
-                    break;
-                }
-            }
-        } while (!Character.isDigit(k3));
-
-        int start = Integer.parseInt(naturalNum1);
-        int end = Integer.parseInt(naturalNum2);
-        int step = Integer.parseInt(naturalStep);
-        System.out.print("Получен ряд чисел: " + start + " ");
+    public static String rowWithStep(int start, int end, int step) {
+        StringBuilder stringRow = new StringBuilder();
         while (start < end) {
             start = start + step;
-                System.out.print(start + " ");
+                stringRow.append(start).append(" ");
         }
-        System.out.println(" ");
+        return stringRow.toString();
     }
 
-    public static void turnNumber_1_5_6() {
-        String n;
-        int length;
-        char k = 0;
-        do {
-            Scanner scn = new Scanner(System.in);
-            System.out.println("Введите целое положительное число");
-            n = scn.nextLine();
-            length = n.length();
-            for (int i = 0; i < length; i++) {
-                k = n.charAt(i);
-                if (!Character.isDigit(k)) {
-                    System.out.println("Введены некорректные данные");
-                    break;
-                }
-            }
-        } while (!Character.isDigit(k));
-
-        int a = Integer.parseInt(n);
-        int turnNum;
+    public static int turnNumber(int a) {
+        int turnNum = 0;
         while (a > 0) {
             turnNum = a % 10;
             a = a / 10;
-            System.out.print(turnNum);
         }
+        return turnNum;
     }
 }
 
